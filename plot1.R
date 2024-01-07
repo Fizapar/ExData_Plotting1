dc52f67a7bb711f56d79a70d5f6dfc6dc838b392
Fiza <24170@yenepoya.edu.in>
2024-01-07 19:07:28 +0530

    Add plots and R scripts

---
+++ plot1.R
@@ -0,0 +1,13 @@
+main_data <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+main_data$Date <- as.Date(main_data$Date, format="%d/%m/%Y")
+
+data <- subset(main_data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(main_data)
+
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
